3b07af4f3580af9fa9b183dcd7d737f534d4b229
admin <yejiangongzr@126.com>
2018-12-24 13:28:15 +0800

    本地工程添加到git

---
+++ git/src/main/java/firstClass.java
@@ -0,0 +1,11 @@
+public class firstClass {
+    public static void main(String[] args) {
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+        System.out.println("helloGit");
+    }
+}
